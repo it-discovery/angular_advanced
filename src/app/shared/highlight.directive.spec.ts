afa4e7173b8940cf278b23dfdef23317a03baf49
Sergiy <sergey.morenets@gmail.com>
2024-11-02 16:30:24 +0200

    Task 15.3. Fix tests for components that use ElementRef

--- src/app/shared/highlight.directive.spec.ts
+++ src/app/shared/highlight.directive.spec.ts
@@ -1,11 +1,15 @@
 import {HighlightDirective} from './highlight.directive';
 import {TestBed} from "@angular/core/testing";
+import {ElementRef} from "@angular/core";
+
+class MockElementRef extends ElementRef {
+}
 
 describe('HighlightDirective', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [HighlightDirective],
+      providers: [HighlightDirective, {provide: ElementRef, useValue: MockElementRef}],
     }).compileComponents();
   });
   it('should create an instance', () => {
