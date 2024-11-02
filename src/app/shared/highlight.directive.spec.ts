adbda82eeb39746bec553b00c4755dcc29cb1ae3
Sergiy <sergey.morenets@gmail.com>
2024-11-02 16:00:20 +0200

    Task 15.1. Fix compilation errors in the tests

--- src/app/shared/highlight.directive.spec.ts
+++ src/app/shared/highlight.directive.spec.ts
@@ -1,8 +1,17 @@
 import {HighlightDirective} from './highlight.directive';
+import {TestBed} from "@angular/core/testing";
+import {ElementRef} from "@angular/core";
 
 describe('HighlightDirective', () => {
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [HighlightDirective],
+      providers: [ElementRef]
+    })
+  });
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const directive = TestBed.inject(HighlightDirective);
     expect(directive).toBeTruthy();
   });
 });
